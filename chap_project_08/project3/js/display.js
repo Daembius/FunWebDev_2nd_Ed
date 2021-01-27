44dfdb2120e749f3c556fbe9ed368a4e25e9b036
Daembius <julienmayard@me.com>
2021-01-27 21:38:56 +0700

    Erase unnecessary comments

--- chap_project_08/project3/js/display.js
+++ chap_project_08/project3/js/display.js
@@ -2,7 +2,6 @@
   // alert("Début de l'IIFE")
   for (let i= 0; i < books.length; i++) {
     let b = books[i];
-    // console.log(b);
     b.outputCard();
   }
 }) ();
--- chap_project_08/project3/js/functions.js
+++ chap_project_08/project3/js/functions.js
@@ -15,11 +15,9 @@ function Book (isbn, title, description, universities) {
     document.write('<div class="mdl-cell mdl-card mdl-shadow--2dp">');
     document.write('<div class="mdl-card__media">');
     document.write("<img  src='images/" + this.isbn + ".jpg' title='this.title'>");
-    // console.log(this.title);
     document.write("</div>");
     document.write('<div class="mdl-card__supporting-text"><p>');
     document.write("<p>"+ this.description + "</p>");
-    // console.log(this.description);
     document.write("<h6>Adopters</h6>");
     document.write("<ul>");
     document.write("<li>" + this.universities + "</li> ");
@@ -28,25 +26,3 @@ function Book (isbn, title, description, universities) {
     document.write('<div class="mdl-card__actions mdl-card--border"><a class="mdl-button">READ MORE</a></div></div>');
   }
 }
-
-
-/*
-Add a function named putputCard () as a prototype function to the Book function object:
-Book.prototype.outputCard = function () {
-
-  document.write('<div class="mdl-cell mdl-card mdl-shadow--2dp">');
-  document.write('<div class="mdl-card__media">');
-  document.write("<img  src='images/" + this.isbn + ".jpg' title='this.title'>");
-  // console.log(this.title);
-  document.write("</div>");
-  document.write('<div class="mdl-card__supporting-text"><p>');
-  document.write("<p>"+ this.description + "</p>");
-  // console.log(this.description);
-  document.write("<h6>Adopters</h6>");
-  document.write("<ul>");
-  document.write("<li>" + this.universities + "</li> ");
-  document.write("</ul>");
-  document.write('</div>');
-  document.write('<div class="mdl-card__actions mdl-card--border"><a class="mdl-button">READ MORE</a></div></div>');
-}
-*/
