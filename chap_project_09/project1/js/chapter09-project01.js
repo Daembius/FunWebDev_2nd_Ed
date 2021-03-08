d19573694741f9fabf98d51af507ce323fc96029
Daembius <julienmayard@me.com>
2021-03-08 14:13:25 +0900

    Highlight form field - focus & blur events - works.

--- chap_project_09/project1/js/chapter09-project01.js
+++ chap_project_09/project1/js/chapter09-project01.js
@@ -2,15 +2,13 @@
 /* add code here  */
 function setBackground(e) {
   if (e.type == "focus") {
-    e.target.style.backgroundColor = "#FFE393";
-    // e.className = 'highlight';
+    e.target.className = "highlight";
   }
   else if (e.type == "blur") {
-    e.target.style.backgroundColor = "white";
+    e.target.className = "hilightable";
   }
 }
 window.addEventListener("load", function(){
-  // let cssSelector = "input[type=text],input[type=password]";
   let cssSelector = ".hilightable";
   let fields = document.querySelectorAll(cssSelector);
   for (i=0; i<fields.length; i++)
