d73087a6333682c837f3c7016d58afb14a234e61
Daembius <julienmayard@me.com>
2021-05-28 20:40:08 +0900

    Chapter 9, project 1 & 2 : Final version

--- chap_project_09/project1/js/chapter09-project01.js
+++ chap_project_09/project1/js/chapter09-project01.js
@@ -1,34 +1,29 @@
 "use strict";
 /* add code here  */
 
-// I. Highlight chosen form field
-function setBackground(e) {
-  if (e.type == "focus") {
-    e.target.className = "highlight";
-  }
-  else if (e.type == "blur") {
-    e.target.className = "hilightable";
-  }
-}
 window.addEventListener("load", function(){
-  // let fields = document.querySelectorAll(".hilightable");
+  // I. Highlight the current field
+  function setBackground(e) {
+    if (e.type == "focus") {
+      e.target.className = "highlight";
+    }
+    else if (e.type == "blur") {
+      e.target.className = "hilightable";
+    }
+  }
+
   let fields = document.getElementsByClassName("hilightable");
   for (let i=0; i<fields.length; i++)
   {
     fields[i].addEventListener("focus", setBackground);
     fields[i].addEventListener("blur", setBackground);
   }
-  });
   // II. Prevent form validation if required fields are empty
 
-  /* responsible for setting up event listeners on page */
-  function init() {
+
+  (function () {
     document.getElementById("mainForm").addEventListener("submit", checkForEmptyFields);
-  }
-
-  /* initialize handlers once page is ready */
-  window.addEventListener("load", init);
-
+  }) ();
   /* ensures form fields are not empty */
   function checkForEmptyFields(e) {
     let fields = document.getElementsByClassName("required");
@@ -39,9 +34,9 @@ window.addEventListener("load", function(){
       if (fields[i].value == null || fields[i].value == "") {
         // since a field is empty prevent the form submission
         e.preventDefault();
-        fields[i].className = "error";
-        console.log(fields[i].value);
-        // fields[i].classList.toggle("error");
+        // fields[i].className = "error";
+        fields[i].classList.toggle("error");
       }
     }
   }
+});
--- chap_project_09/project2/js/chapter09-project02.js
+++ chap_project_09/project2/js/chapter09-project02.js
@@ -1,3 +1,27 @@
-/* add code here */
+"use strict";
 
-window.addEventListener("load", init);
+window.addEventListener("load", function(){
+
+  const thumbs = document.getElementById("thumbnails");
+  const featuredImage = document.querySelector("#featured img");
+  const caption = document.querySelector("#featured figcaption");  // essayer aussi const caption = document.querySelector("#figcaption");
+
+  // Replace clicked thumbnail by large picture
+  thumbs.addEventListener("click", function (e) {
+    if (e.target.nodeName.toLowerCase() == 'img') {
+      let clickedImageSource = e.target.src;
+      let newSrc = clickedImageSource.replace("small", "medium");
+      featuredImage.src = newSrc;
+      caption.textContent = e.target.title;
+    }
+  });
+featuredImage.addEventListener("mouseover", function (event){
+  caption.style.opacity = "0.8";
+  caption.style.transition = "0.5";
+});
+featuredImage.addEventListener("mouseout", function (event) {
+  caption.style.opacity =  "0";
+  caption.style.transition = "1.5s";
+})
+
+});
